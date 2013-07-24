a2568550e7352e468046d46b46d2e74d47e47b66
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2013-07-24 15:00:06 -0500

    added noted to download with https://github.com/i5K-KINBRE-script-share/transcriptome-and-genome-assembly/blob/master/KSU_bioinfo_lab/Count_fastas.pl

--- KSU_bioinfo_lab/assembly_quality_stats_for_multiple_assemblies.pl
+++ KSU_bioinfo_lab/assembly_quality_stats_for_multiple_assemblies.pl
@@ -4,7 +4,7 @@
 #  
 #
 #  Created by jennifer shelton on 7/24/13.
-# This script runs a slightly modified version of Joseph Fass' Count_fasta.pl (original available at http://wiki.bioinformatics.ucdavis.edu/index.php/Count_fasta.pl ) on a fasta file from each assembly. It then creates comma separated file called assembly_metrics.txt listing the N25,N50,N75, cumulative contig length, and number of contigs for each assembly.
+# This script runs a slightly modified version of Joseph Fass' Count_fasta.pl (original available at http://wiki.bioinformatics.ucdavis.edu/index.php/Count_fasta.pl ) on a fasta file from each assembly. It then creates comma separated file called assembly_metrics.txt listing the N25,N50,N75, cumulative contig length, and number of contigs for each assembly. To use also download Count_fastas.pl https://github.com/i5K-KINBRE-script-share/transcriptome-and-genome-assembly/blob/master/KSU_bioinfo_lab/Count_fastas.pl and change $path_to_Count_fastas on line 13 of assembly_quality_stats_for_multiple_assemblies.pl.
 # usage: assembly_quality_stats_for_multiple_assemblies.pl [fasta_file or files]
 
 use strict;
@@ -16,7 +16,7 @@ open (METRICS, ">$outfile");
 print METRICS "Filename,Cumulative length of contigs(bp),Number of contigs,N25(bp),N50(bp),N75(bp),Number of contigs longer than N25,Number of contigs longer than N50,Number of contigs longer than N75\n";
 foreach my $f (@ARGV)
 {
-		my $out=`perl ${path_to_Count_fastas}/Count_fastas.pl $f`;
+		my $out=`perl ${path_to_Count_fastas}/Count_fastas.pl ${f}`;
 		print METRICS "${f},${out}\n";
 
 }
