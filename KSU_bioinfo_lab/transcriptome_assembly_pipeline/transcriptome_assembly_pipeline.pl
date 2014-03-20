f409106697015cf3ebd5740f3de078d042cf2890
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-03-20 10:04:27 -0500

    debug

--- KSU_bioinfo_lab/transcriptome_assembly_pipeline/transcriptome_assembly_pipeline.pl
+++ KSU_bioinfo_lab/transcriptome_assembly_pipeline/transcriptome_assembly_pipeline.pl
@@ -32,7 +32,7 @@ print "########################################################################\
 ##############                get arguments                  ##################
 ###############################################################################
 my ($r_list,$clean_read_file1,$clean_read_file2,$clean_read_singletons);
-my $project_name = "my_project";,
+my $project_name = "my_project";
 my $convert_header = 0;
 my $shortest_k = 25,
 my $longest_k = 65,
@@ -154,7 +154,7 @@ for my $samples (@reads)
     print QSUBS_SINGLEK "#!/bin/bash\n";
 
 
-    for ( my $k = $shortest_k; $i =< $longest_k; $i += $increment_k )
+    for ( my $k = $shortest_k; $k <= $longest_k; $k += $increment_k )
     {
 
         close (SCRIPT);
